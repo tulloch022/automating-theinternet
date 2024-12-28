4667b12f9574008d3ac63ec43b70b0754b491d15
tulloch022 <90489018+tulloch022@users.noreply.github.com>
2024-12-28 13:58:27 -0800

    added comments to add/remove elements test

--- tests/add-remove-elements.spec.ts
+++ tests/add-remove-elements.spec.ts
@@ -7,13 +7,20 @@ test.beforeEach(async ({ page }) => {
 
 
 test.describe('Add/Remove Elements', () => {
-    test('Add/Remove Elements link should bring me to Add/Remove Elements page', async ({ page }) => {
+    test('Add/Remove Elements button should add an element', async ({ page }) => {
+        // Click the link to navigate to the Add/Remove Elements page
         const link = page.getByText('Add/Remove Elements');
         await link.click()
+
+        // Verify the header of the Add/Remove Elements page is visible
         const header = page.getByRole('heading', {name: 'Add/Remove Elements'})
         await expect(header).toBeVisible();
+
+        // Click the "Add Element" button
         const addElementButton = page.getByText('Add Element');
         await addElementButton.click();
+
+        // Verify the "Delete" button is visible
         const deleteButton = page.getByText('Delete');
         await expect(deleteButton).toBeVisible();
     })
