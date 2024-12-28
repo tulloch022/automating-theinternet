583ae7a28b6920abeb998f0a986688e7278ab813
Kevin Tulloch <kevintulloch@Kevins-MacBook-Air-2.local>
2024-12-28 13:09:27 -0800

    created simple add/remove elements test

--- tests/add-remove-elements.spec.ts
+++ tests/add-remove-elements.spec.ts
@@ -1,3 +1,20 @@
 import { test, expect } from '@playwright/test';
 
 
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+  });
+
+
+test.describe('Add/Remove Elements', () => {
+    test('Add/Remove Elements link should bring me to Add/Remove Elements page', async ({ page }) => {
+        const link = page.getByText('Add/Remove Elements');
+        await link.click()
+        const header = page.getByRole('heading', {name: 'Add/Remove Elements'})
+        await expect(header).toBeVisible();
+        const addElementButton = page.getByText('Add Element');
+        await addElementButton.click();
+        const deleteButton = page.getByText('Delete');
+        await expect(deleteButton).toBeVisible();
+    })
+})
