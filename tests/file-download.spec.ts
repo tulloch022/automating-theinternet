d734b1ab4b13d0614473eb56c1a491997bdb6d33
tulloch022 <90489018+tulloch022@users.noreply.github.com>
2025-01-13 20:11:27 -0800

    fixed file download page text to account for links disappearing

--- tests/file-download.spec.ts
+++ tests/file-download.spec.ts
@@ -19,13 +19,24 @@ test('File Download page should navigate correctly and links should download exp
     const header = page.getByRole('heading', {name: 'File Downloader'});
     await expect(header).toBeVisible();
 
-    // Click canvas image link and confirm download
+    // Locate all file links on the page
+    const fileLinks = page.locator('a[href*="."]'); // Select links containing a file extension
+    const fileCount = await fileLinks.count();
+
+    // Ensure there is at least one file link on the page
+       expect(fileCount).toBeGreaterThan(0);
+
+    // Select the last file link
+    const firstFileLink = fileLinks.nth(1);
+    const fileName = await firstFileLink.textContent();
+
+
     const [download] = await Promise.all([
         page.waitForEvent('download'),
-        page.getByText('canvas_image.png').click()
+        firstFileLink.click()
     ]);
 
-    const targetPath = path.resolve(__dirname, 'assets/test.png');
+    const targetPath = path.resolve(__dirname, `assets/${fileName}`);
     await download.saveAs(targetPath);
 
     const fileExists = fs.existsSync(targetPath);
